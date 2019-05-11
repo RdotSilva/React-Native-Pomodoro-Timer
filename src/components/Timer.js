8ad30e28961265f78baeba458d658b601b991e5a
Ryan Silva <RyandotSilva@gmail.com>
2019-05-11 17:01:04 -0400

    Set up Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 class Timer extends Component {
+	constructor(props) {
+		super(props);
+	}
 	render() {
 		return <div />;
 	}
