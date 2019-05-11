377440ab6327f9e02075cf04960cb5512818b67b
Ryan Silva <RyandotSilva@gmail.com>
2019-05-11 17:02:03 -0400

    Set up initial state in Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,6 +4,10 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 class Timer extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			timeReamining: '25:00',
+			isStarted: false
+		};
 	}
 	render() {
 		return <div />;
