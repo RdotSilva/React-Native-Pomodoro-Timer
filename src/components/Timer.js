4d13b206e19b2688fdc560032e9211ec481d4b91
Ryan Silva <RyandotSilva@gmail.com>
2019-05-11 16:57:40 -0400

    Add Timer.js file

---
+++ src/components/Timer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class Timer extends Component {
+	render() {
+		return <div />;
+	}
+}
+
+export default Timer;
