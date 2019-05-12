3e4aa1460e9d008c9a26852bdc19ede14f6654fb
Ryan Silva <RyandotSilva@gmail.com>
2019-05-12 11:56:00 -0400

    Add method bindings

--- App.js
+++ App.js
@@ -20,113 +20,116 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			currentTime: "25:00",
-			workTime: "25:00",
-			breakTime: "05:00",
+			currentTime: '25:00',
+			workTime: '25:00',
+			breakTime: '05:00',
 			working: true,
 			timer: null,
 			paused: false,
-			playing, false
-		}
+			playing: false
+		};
+		this.setWorkTimer = this.setWorkTimer.bind(this);
+		this.setBreakTimer = this.setBreakTimer.bind(this);
+		this.playButton = this.playButton.bind(this);
+		this.pauseButton = this.pauseButton.bind(this);
+		this.resetButton = this.resetButton.bind(this);
+		this.countdown = this.countdown.bind(this);
+		this.toggleStatus = this.toggleStatus.bind(this);
 	}
 
 	setWorkTimer(x) {
 		let newTime = getTime(x);
 		this.setState({
-		  workTime: newTime,
+			workTime: newTime
 		});
 		if (!this.state.timer) {
-		  this.setState({
-			currentTime: newTime,
-		  });
-		}      
+			this.setState({
+				currentTime: newTime
+			});
+		}
 	}
 
 	setBreakTimer(x) {
 		let newTime = getTime(x);
 		this.setState({
-		  breakTime: newTime,
+			breakTime: newTime
 		});
 	}
 
 	playButton() {
-		if (this.state.paused === true || this.state.playing === false) { 
-		  this.setState({
-			timer: setInterval(this.countdown, 1000),
-			paused: false,
-			playing: true,
-		  });
+		if (this.state.paused === true || this.state.playing === false) {
+			this.setState({
+				timer: setInterval(this.countdown, 1000),
+				paused: false,
+				playing: true
+			});
 		}
 	}
 
-	pauseButton () {
+	pauseButton() {
 		if (this.state.paused === false && this.state.playing === true) {
-		  clearInterval(this.state.timer);
-		  this.setState({
-			paused: true,
-			timer: null,
-			playing: false
-		  });
-		  console.log(this.state.paused);
+			clearInterval(this.state.timer);
+			this.setState({
+				paused: true,
+				timer: null,
+				playing: false
+			});
+			console.log(this.state.paused);
 		} else if (this.state.paused === true && this.state.playing === false) {
-		  this.playButton();
-		}       
+			this.playButton();
+		}
 	}
 
-	resetButton () {
+	resetButton() {
 		this.pauseButton();
 		this.setState({
-		  currentTime: this.state.workTime,
-		  playing: false,
-		  paused: false,
-		  working: true,
-		})
+			currentTime: this.state.workTime,
+			playing: false,
+			paused: false,
+			working: true
+		});
 	}
 
 	countdown() {
-		if (this.state.currentTime === "00:00" && this.state.playing === true) {
-		  console.log('finished');
-		  vibrate();
-		  this.toggleStatus();
+		if (this.state.currentTime === '00:00' && this.state.playing === true) {
+			console.log('finished');
+			vibrate();
+			this.toggleStatus();
 		} else {
-		  let sec = this.state.currentTime.slice(3);
-		  let min = this.state.currentTime.slice(0, 2);
-		  if (sec === "00") {
-			let newMin = formatTime(parseInt(min) - 1);
-			let newTime = newMin + ":59";
-			this.setState({
-			  currentTime: newTime,
-			});
-		  } else {
-			let newSec = formatTime((parseInt(sec) - 1));
-			let newTime = min + ":" + newSec;
-			this.setState({
-			  currentTime: newTime,
-			})
-		  }
+			let sec = this.state.currentTime.slice(3);
+			let min = this.state.currentTime.slice(0, 2);
+			if (sec === '00') {
+				let newMin = formatTime(parseInt(min) - 1);
+				let newTime = newMin + ':59';
+				this.setState({
+					currentTime: newTime
+				});
+			} else {
+				let newSec = formatTime(parseInt(sec) - 1);
+				let newTime = min + ':' + newSec;
+				this.setState({
+					currentTime: newTime
+				});
+			}
 		}
 	}
 
 	toggleStatus() {
 		if (this.state.working) {
-		  this.setState({
-			working: false,
-			currentTime: this.state.breakTime,
-		  })
+			this.setState({
+				working: false,
+				currentTime: this.state.breakTime
+			});
 		} else {
-		  this.setState({
-			working: true,
-			currentTime: this.state.workTime,
-		  })
+			this.setState({
+				working: true,
+				currentTime: this.state.workTime
+			});
 		}
 	}
 
 	render() {
-		return (
-			<div>
-
-			</div>
-		);
+		return <div />;
 	}
 }
 
