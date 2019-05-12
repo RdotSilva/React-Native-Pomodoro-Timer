84715135046fe73f2b29dd1f4c26c5bc77471e9a
Ryan Silva <RyandotSilva@gmail.com>
2019-05-12 11:30:18 -0400

    Add style for Buttons component

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 
+const style = StyleSheet.create({
+	button: {
+		marginHorizontal: 5,
+		marginBottom: 20
+	}
+});
+
 export default class Buttons extends Component {
 	render() {
 		return (
