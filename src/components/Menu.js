a2d5ed39729a397579e1103ed6d0df60b94fe5a6
Ryan Silva <RyandotSilva@gmail.com>
2019-05-12 12:11:33 -0400

    Add style for Menu component

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import { Picker, StyleSheet } from 'react-native';
 
+const style = StyleSheet.create({
+	menu: {
+		marginLeft: 10,
+		marginRight: 10,
+		width: 90,
+		height: 35
+	}
+});
+
 let list = [];
 for (let i = 0; i < 60; i++) {
 	list.push(i.toString());
@@ -10,7 +19,7 @@ export default class Menu extends Component {
 	render() {
 		return (
 			<Picker
-				style={styles.menu}
+				style={style.menu}
 				mode="dropdown"
 				onValueChange={this.props.onValueChange}
 				selectedValue={this.props.selected}
